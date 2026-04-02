00a3e767bffa602d4a5c92e419e54370b8259706
Tarik Filipović <tarikfilipovic1234@gmail.com>
2026-04-02 15:08:07 +0200

    End of section 4

---
+++ Application/Activities/Commands/CreateActivity.cs
@@ -0,0 +1,28 @@
+using Activity = Domain.Activity;
+using System;
+using System.Diagnostics;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities.Commands;
+
+public class CreateActivity
+{
+    public class Command : IRequest<string>
+    {
+        
+        public required Activity Activity {get; set;}
+    }
+
+    public class Handler(AppDbContext context) : IRequestHandler<Command, string>
+    {
+        public async Task<string> Handle(Command request, CancellationToken cancellationToken)
+        {
+            context.Activities.Add(request.Activity);
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return request.Activity.Id;
+        }
+    }
+}
---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Diagnostics;
+using AutoMapper;
+using Activity = Domain.Activity;
+
+namespace Application.Core;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Activity, Activity>();
+    }
+}
