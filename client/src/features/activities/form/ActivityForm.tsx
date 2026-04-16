0b1d31425eefac4dda120e1e7f0fdb09a1b60941
Tarik Filipović <tarikfilipovic1234@gmail.com>
2026-04-16 16:46:14 +0200

    End of section 7

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,11 @@
 import { Paper, Typography, Box, TextField, Button } from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
+import { useNavigate, useParams } from "react-router";
 
-type Props = {
-    activity?: Activity;
-    closeForm: () => void;
-};
-
-export default function ActivityForm({ activity, closeForm }: Props) {
-    const { updateActivity, createActivity } = useActivities();
+export default function ActivityForm() {
+    const {id} = useParams();
+    const { updateActivity, createActivity, activity, isLoadingActivity } = useActivities(id);
+    const navigate = useNavigate();
 
     const handleSubmit = async (event: React.SyntheticEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -22,13 +20,18 @@ export default function ActivityForm({ activity, closeForm }: Props) {
         if (activity) {
             data.id = activity.id;
             await updateActivity.mutateAsync(data as unknown as Activity);
-            closeForm();
+            navigate(`/activities/${activity.id}`);
         } else {
             data.id = crypto.randomUUID();
-            await createActivity.mutateAsync(data as unknown as Activity);
-            closeForm();
+            await createActivity.mutate(data as unknown as Activity, {
+                onSuccess: (id) => {
+                    navigate(`/activities/${id}`)
+                }
+            }); 
         }
     };
+
+    if (isLoadingActivity) return <Typography>Loading activity...</Typography>
 
     return (
         <Paper sx={{ borderRadius: 3, padding: 3 }}>
@@ -88,7 +91,7 @@ export default function ActivityForm({ activity, closeForm }: Props) {
                 />
 
                 <Box display="flex" justifyContent="end" gap={3}>
-                    <Button onClick={closeForm} color="inherit">
+                    <Button color="inherit">
                         Cancel
                     </Button>
                     
